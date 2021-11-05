506637896c8b2ea6e6fc805440ce739802579834
Vineetha <2020MT93718@wilp.bits-pilani.ac.in>
2021-11-05 23:36:12 +0530

    Input value changes

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,17 @@ public class Calculator
 
     public static void main(String[] args) 
     {
-        int num1 =5;
-        int num2 =6;
+        //int num1 =5;
+        //int num2 =6;
+        
+
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
         
         new Addition().add(num1,num2);
         new Subtraction().difference(num1,num2);
