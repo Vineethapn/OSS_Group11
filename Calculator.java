839996b8cc2b91d116a7363d790c49811e24ac79
Vineetha <2020MT93718@wilp.bits-pilani.ac.in>
2021-11-07 12:09:44 +0530

    Taking input operation from user

--- Calculator.java
+++ Calculator.java
@@ -7,18 +7,28 @@ public class Calculator
         //int num2 =6;
         
 
-        int num1, num2, sum;
+        int num1, num2, sum, operation; 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number: ");
         num1 = sc.nextInt();
         
         System.out.println("Enter Second Number: ");
         num2 = sc.nextInt();
-        
+
+        System.out.println("Enter the operation you want to perform 1.Add 2.Sub 3.Mul 4.Div");
+        operation = sc.nextLine();
+
+        if(operation == "Add"){
         new Addition().add(num1,num2);
+        }else if(operation == "Sub"){
         new Subtraction().difference(num1,num2);
+        }else if(operation == "Mul"){
         new Multiplication().product(num1,num2);
+        }else if(operation == "Div"){
         new Division().ratio(num1,num2);
+        }else{
+            System.out.println("Please enter valid input");
+        }
         }
 
     }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OSS_Group11
  Assignment_4:  Basic Calculator Implementation
+
+# Vineetha 
+    Added code for taking the input from user for the operation to be performed.
